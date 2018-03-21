7f48af9a46aab5469b9646364e956b3ed7625021
liuzhixian <956295973@qq.com>
2018-03-21 15:53:41 +0800

    代码榜滚动加载

--- js/code-rank.js
+++ js/code-rank.js
@@ -2,6 +2,12 @@ $(function () {
     var rankPageIndex = 0;
     var tempRankData = [];
     var monthNum = 3;
+    var range = 0;             //距下边界长度/单位px
+    var elemt = 500;           //插入元素高度/单位px
+    var maxnum = 30;            //设置加载最多次数
+    var num = 1;
+    var totalheight = 0;
+    var isLoaded = true
     window.sessionStorage.removeItem('rankList');
     $(".search-click-hook").on('click',function () {
         console.log('点击搜索');
@@ -24,13 +30,13 @@ $(function () {
         }
         for(i=1;i<=10;i++){
             var rankIndex = rankPageIndex*10+i;
-            rankData[rankIndex].sortNum = rankIndex;
-            tempRank.push(rankData[rankIndex]);
             if(rankIndex>=len){
                 $(".no-more-hook").fadeIn();
                 $(".load-more-hook").hide();
                 return tempRank;
             }
+            rankData[rankIndex].sortNum = rankIndex;
+            tempRank.push(rankData[rankIndex]);
         }
         return tempRank;
     }
@@ -38,46 +44,85 @@ $(function () {
     function getRankData(){
         $(".no-data-tip").fadeOut();
         $(".waiting-data").fadeIn();
-        doRankGet('?month='+monthNum, function(data) {
-            if(data != null) {
-                var rankData;
-                var cometSeesionStore = window.sessionStorage.getItem('rankList')
-                if(cometSeesionStore == null){
-                    window.sessionStorage.setItem('rankList',JSON.stringify(data));
-                    rankData = data;
-                }else{
-                    rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
-                }
-                if(rankData.length == 0){
-                    $(".load-more-hook").hide();
+        var rankData;
+        var cometSeesionStore = window.sessionStorage.getItem('rankList')
+        if(cometSeesionStore == null){
+            doRankGet('?month='+monthNum, function(data) {
+                rankData = data;
+                window.sessionStorage.setItem('rankList',JSON.stringify(data));
+                if(rankData != null) {
+                    if(rankData.length == 0){
+                        $(".load-more-hook").hide();
+                        $(".waiting-data").hide();
+                        $(".no-data-tip").fadeIn();
+                    }else{
+                        tempRankData =  getTenRankData(rankData);
+                        var rankTpl = $("#rank-item-temp").html();
+                        var content = template(rankTpl, {list: tempRankData});
+                        $(".code-rank-wrap").append(content);
+                        $(".no-data-tip").fadeOut()
+                        $(".waiting-data").hide();
+                        //$(".load-more-hook").show();
+                    }
+                } else{
+                    //$(".load-more-hook").hide();
                     $(".waiting-data").hide();
-                    $(".no-data-tip").fadeIn();
-
-                }else{
-                    tempRankData =  getTenRankData(rankData);
-                    var rankTpl = $("#rank-item-temp").html();
-                    var content = template(rankTpl, {list: tempRankData});
-                    $(".code-rank-wrap").append(content);
-                    $(".no-data-tip").fadeOut()
-                    $(".waiting-data").hide();
-                    $(".load-more-hook").show();
+                    $(".no-data-tip").show()
                 }
-            } else{
+            }, "json");
+        }else{
+            rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
+            renderCodeList(rankData)
+        }
+    }
+    function renderCodeList(rankData) {
+        if(rankData != null) {
+            if(rankData.length == 0){
                 $(".load-more-hook").hide();
                 $(".waiting-data").hide();
-                $(".no-data-tip").show()
+                $(".no-data-tip").fadeIn();
+            }else{
+                tempRankData =  getTenRankData(rankData);
+                var rankTpl = $("#rank-item-temp").html();
+                var content = template(rankTpl, {list: tempRankData});
+                $(".code-rank-wrap").append(content);
+                $(".no-data-tip").fadeOut()
+                $(".waiting-data").hide();
+                //$(".load-more-hook").show();
             }
-        }, "json");
+        } else{
+            //$(".load-more-hook").hide();
+            $(".waiting-data").hide();
+            $(".no-data-tip").show()
+        }
     }
+    //滚动加载
+    $(window).scroll(function(){
+        var srollPos = $(window).scrollTop();    //滚动条距顶部距离(页面超出窗口的高度)
+        // console.log("滚动条到顶部的垂直高度: "+$(document).scrollTop());
+        //console.log("页面的文档高度 ："+$(document).height());
+        //console.log('浏览器的高度：'+$(window).height());
+        totalheight = parseFloat($(window).height()) + parseFloat(srollPos);
+        if(($(document).height()-range) <= totalheight  && num != maxnum && isLoaded) {
+            isLoaded = false
+            num = num + 1;
+            loadCodeRank();
+        }
+    });
+   /* $(".load-more-hook .loading-more").on('click',function () {
 
-    $(".load-more-hook .loading-more").on('click',function () {
+    })*/
+    function  loadCodeRank() {
         $(".loading-more").hide();
         $(".loader1").css('display','flex');
         rankPageIndex++;
         var rankData;
         var cometSeesionStore = window.sessionStorage.getItem('rankList')
         if(cometSeesionStore == null){
-            window.sessionStorage.setItem('rankList',JSON.stringify(data))
+            doRankGet('?month='+monthNum, function(data) {
+                rankData = data;
+                window.sessionStorage.setItem('rankList',JSON.stringify(data));
+            }, "json");
         }else{
             rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
         }
@@ -87,8 +132,8 @@ $(function () {
         $(".code-rank-wrap").append(content)
         $(".loading-more").show();
         $(".loader1").css('display','none');
-
-    })
+        isLoaded = true;
+    }
 })
 
 
