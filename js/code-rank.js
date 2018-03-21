1b6b64f3eb5a7354a490205079046c971e1f93d8
liuzhixian <956295973@qq.com>
2018-03-21 14:50:00 +0800

    代码榜

--- js/code-rank.js
+++ js/code-rank.js
@@ -1,12 +1,27 @@
 $(function () {
+    var rankPageIndex = 0;
+    var tempRankData = [];
+    var monthNum = 3;
+    window.sessionStorage.removeItem('rankList');
     $(".search-click-hook").on('click',function () {
         console.log('点击搜索');
     })
-    var rankPageIndex = 0;
-    var tempRankData = [];
+    $(".title-select-wrap .item").on('click',function () {
+        var self = $(this);
+        self.parent().parent().find('.item').removeClass('item-active');
+        self.addClass('item-active')
+        monthNum = self.data('month');
+        rankPageIndex = 0;
+        window.sessionStorage.removeItem('rankList')
+        $(".code-rank-wrap").html('');
+        getRankData();
+    })
     function getTenRankData(rankData){
         var tempRank = [];
         var len = rankData.length-1;
+        if(len<=0){
+            return tempRank;
+        }
         for(i=1;i<=10;i++){
             var rankIndex = rankPageIndex*10+i;
             rankData[rankIndex].sortNum = rankIndex;
@@ -19,25 +34,42 @@ $(function () {
         }
         return tempRank;
     }
-    doRankGet('', function(data) {
-        if(data != null) {
-            var rankData;
-            var cometSeesionStore = window.sessionStorage.getItem('rankList')
-            if(cometSeesionStore == null){
-                window.sessionStorage.setItem('rankList',JSON.stringify(data));
-                rankData = data;
-            }else{
-                rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
+    getRankData();
+    function getRankData(){
+        $(".no-data-tip").fadeOut();
+        $(".waiting-data").fadeIn();
+        doRankGet('?month='+monthNum, function(data) {
+            if(data != null) {
+                var rankData;
+                var cometSeesionStore = window.sessionStorage.getItem('rankList')
+                if(cometSeesionStore == null){
+                    window.sessionStorage.setItem('rankList',JSON.stringify(data));
+                    rankData = data;
+                }else{
+                    rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
+                }
+                if(rankData.length == 0){
+                    $(".load-more-hook").hide();
+                    $(".waiting-data").hide();
+                    $(".no-data-tip").fadeIn();
+
+                }else{
+                    tempRankData =  getTenRankData(rankData);
+                    var rankTpl = $("#rank-item-temp").html();
+                    var content = template(rankTpl, {list: tempRankData});
+                    $(".code-rank-wrap").append(content);
+                    $(".no-data-tip").fadeOut()
+                    $(".waiting-data").hide();
+                    $(".load-more-hook").show();
+                }
+            } else{
+                $(".load-more-hook").hide();
+                $(".waiting-data").hide();
+                $(".no-data-tip").show()
             }
-            tempRankData =  getTenRankData(rankData);
-            var rankTpl = $("#rank-item-temp").html();
-            var content = template(rankTpl, {list: tempRankData});
-            $(".code-rank-wrap").append(content);
-            $(".load-more-hook").show();
-        } else{
-            alert('请求数据出错');
-        }
-    }, "json");
+        }, "json");
+    }
+
     $(".load-more-hook .loading-more").on('click',function () {
         $(".loading-more").hide();
         $(".loader1").css('display','flex');
@@ -58,3 +90,6 @@ $(function () {
 
     })
 })
+
+
+
