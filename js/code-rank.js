be86f2046db5bd575d677850f27ed4ca67c3b62c
liuzhixian <956295973@qq.com>
2018-03-22 14:58:21 +0800

    代码榜数据问题处理

--- js/code-rank.js
+++ js/code-rank.js
@@ -7,7 +7,8 @@ $(function () {
     var maxnum = 30;            //设置加载最多次数
     var num = 1;
     var totalheight = 0;
-    var isLoaded = true
+    var isLoaded = true;
+    var pageSize = 20;
     window.sessionStorage.removeItem('rankList');
     $(".search-click-hook").on('click',function () {
         console.log('点击搜索');
@@ -28,8 +29,8 @@ $(function () {
         if(len<=0){
             return tempRank;
         }
-        for(i=0;i<10;i++){
-            var rankIndex = rankPageIndex*10+i;
+        for(i=0;i<pageSize;i++){
+            var rankIndex = rankPageIndex*pageSize+i;
             if(rankIndex>=len){
                 $(".no-more-hook").fadeIn();
                 $(".load-more-hook").hide();
@@ -50,7 +51,7 @@ $(function () {
             doRankGet('?month='+monthNum, function(data) {
                 rankData = data;
                 window.sessionStorage.setItem('rankList',JSON.stringify(data));
-                if(rankData != null) {
+               // if(rankData != null) {
                     if(rankData.length == 0){
                         $(".load-more-hook").hide();
                         $(".waiting-data").hide();
@@ -64,17 +65,18 @@ $(function () {
                         $(".waiting-data").hide();
                         //$(".load-more-hook").show();
                     }
-                } else{
+             /*   } else{
                     //$(".load-more-hook").hide();
                     $(".waiting-data").hide();
                     $(".no-data-tip").show()
-                }
+                }*/
             }, "json");
         }else{
             rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
             renderCodeList(rankData)
         }
     }
+    //首次切换和第一次页面加载
     function renderCodeList(rankData) {
         if(rankData != null) {
             if(rankData.length == 0){
@@ -103,7 +105,7 @@ $(function () {
         //console.log("页面的文档高度 ："+$(document).height());
         //console.log('浏览器的高度：'+$(window).height());
         totalheight = parseFloat($(window).height()) + parseFloat(srollPos);
-        if(($(document).height()-range) <= totalheight  && num != maxnum && isLoaded) {
+        if(($(document).height()-range) <= totalheight  && num != maxnum && num > 0 && isLoaded) {
             isLoaded = false
             num = num + 1;
             loadCodeRank();
@@ -112,27 +114,36 @@ $(function () {
    /* $(".load-more-hook .loading-more").on('click',function () {
 
     })*/
+   //滚动加载
     function  loadCodeRank() {
         $(".loading-more").hide();
         $(".loader1").css('display','flex');
-        rankPageIndex++;
         var rankData;
         var cometSeesionStore = window.sessionStorage.getItem('rankList')
         if(cometSeesionStore == null){
+            //首次进入页面加载
             doRankGet('?month='+monthNum, function(data) {
                 rankData = data;
                 window.sessionStorage.setItem('rankList',JSON.stringify(data));
+                srcollToLoadData(rankData)
             }, "json");
         }else{
+            //缓存加载
             rankData = JSON.parse(window.sessionStorage.getItem('rankList'));
+            srcollToLoadData(rankData)
         }
+        $(".loading-more").show();
+        $(".loader1").css('display','none');
+        isLoaded = true;
+    }
+    function  srcollToLoadData(rankData) {
+        ++rankPageIndex ;
+        console.log(rankPageIndex)
         tempRankData =  getTenRankData(rankData);
         var rankTpl = $("#rank-item-temp").html();
         var content = template(rankTpl, {list: tempRankData});
         $(".code-rank-wrap").append(content)
-        $(".loading-more").show();
-        $(".loader1").css('display','none');
-        isLoaded = true;
+
     }
 })
 
