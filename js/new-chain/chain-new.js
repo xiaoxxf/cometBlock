68cc936dc73c6c187711fc8089b82dd5ecf19e87
wuzhao <liwuzhao97@gmail.com>
2018-03-27 17:02:53 +0800

    接口调试

--- js/new-chain/chain-new.js
+++ js/new-chain/chain-new.js
@@ -28,7 +28,7 @@ $('.white_paper').on('change', function(e){
 })
 
 // 增加发行价格
-function add_input()
+function add_issue_price()
 {
 	var div = '<div class="form-group row"><label class="col-xs-12 col-md-2 col-sm-2 control-label"></label><div class="col-xs-12 col-md-5 col-sm-5"><div class="input-group"><span class="input-group-addon"><img src="img/bitcoin.png"/ style="height: 20px;"></span><input type="text" name="issue_price" class="form-control"></div></div></div>'
 	$('#add_issue_price').append(div);
@@ -56,16 +56,8 @@ var getOnloadFunc = function(aImg) {
 	};
 }
 
-var imageMaxSize = 2*1024*1024;
-var whitePaperMaxSize = 2*1024*1024*10;
-var imageType = /image.*/;
 var input_project_logo = document.getElementById("project_logo_input");
 
-var getOnloadFunc = function(aImg) {
-	return function(evt) {
-		aImg.src = evt.target.result;
-	};
-}
 input_project_logo.addEventListener("change", function() {
 	$(".coin_image_box").html("");
 //			    for (var i = 0, numFiles = this.files.length; i < numFiles; i++) {
@@ -81,79 +73,8 @@ input_project_logo.addEventListener("change", function() {
     reader.readAsDataURL(file);
 //			    }
 }, false);
-
-
-//表单校验
-var pdfType = /pdf.*/;
-var imageType = /image.*/;
-var imageMaxSize = 2*1024*1024;
-var pdfMaxSize  = 2*1024*1024*10;
-var flag = true;
-$('#form1').validator({
-	theme:'bootstrap',
-    validClass: "has-succes",
-    invalidClass: "has-error",
-    bindClassTo: ".form-group",
-
-    rules: {
-       imageTypeAndSize:function(element, params,field) {
-           var filepath = element.files[0];
-           if (!filepath.type.match(imageType) || filepath.size > imageMaxSize){
-           	 return "上传有误";
-           }else {
-              return true;
-           }
-       },
-       pdfTypeAndSize:function(element, params,field) {
-           var filepath = element.files[0];
-           if (!filepath.type.match(pdfType) || filepath.size > imageMaxSize){
-           	flag = false
-           	return "上传有误";
-           }else {
-         	flag = true;
-            return true;
-           }
-        },
-        pdfName:function(element){
-        		return flag || '请上传不大于20M的PDF文件';
-        },
-        teamNumber:function(element, params, field){
-          var numbers = element;
-          if (numbers%3 == 0) {
-            return true;
-          }else {
-            return '请完整地填写团队成员的信息'
-          }
-        }
-
-    },
-
-    fields: {
-    		// 'project_logo': 'required;imageTypeAndSize',
-    		// 'project_name': 'required',
-    		// 'project_big_name': 'required',
-        // 'project_type': 'required',
-    		// 'fundraising_time': 'date',
-    		// 'currency_count': 'required;integer',
-    		// 'compay_website': 'required;url',
-    		// 'block_browser': 'url',
-    		// 'project_content': 'required',
-        // 'currency_circulation': 'required;integer',
-        // 'white_paper': 'pdfTypeAndSize',
-        // 'white_paper_file_name': 'pdfName',
-    }
-});
-
-// 添加团队成员
-function add_team_member()
-{
-  var string = '<div class="col-xs-6 col-md-2 col-sm-3"><div class="team_image_box"><img src="img/no_image.png" class="" /></div><div class="member_mess"><div class=""><a href="javascript:;" class="file" style="text-align:center;text-decoration:none">选择<input type="file"  name="team_member_pic" class="input_team_member_image"></a><a href="javascript:;" class="file">上传<input type="button" class="change" value="上传" name="file" onclick="doUpload(this.parentElement.previousElementSibling.childNodes[1])" style="cursor:pointer"/></a></div><input type="text" class="form-control" name="team_member_name" value="" placeholder="名称"><input type="text" class="form-control " name="team_member_position" value="" placeholder="职位" ></div></div>'
-
-  $('.team').append(string);
-}
-
 // 团队图片上传及预览
-$('.team').on('change', $('.input_team_member_image'), function(e) {
+$('.team').on('change', $('.member_pic'), function(e) {
   if(e.target.type != 'file'){
     return false
   }
@@ -174,13 +95,95 @@ $('.team').on('change', $('.input_team_member_image'), function(e) {
 
 
 
+
+// 添加团队成员
+function add_team_member()
+{
+  var string = '<div class="col-xs-6 col-md-2 col-sm-3"><div class="team_image_box"><img src="img/no_image.png" class="" /></div><div class="member_mess"><div class=""><a href="javascript:;" class="file" style="text-align:center;text-decoration:none">选择<input type="file"  name="file" class="member_pic"></a>  <a href="javascript:;" class="file">上传<input type="button" class="change" value="上传" onclick="doUpload(this.parentElement.previousElementSibling.childNodes[1])" style="cursor:pointer"/></a></div><input type="text" class="form-control member_name" name="member_name" value="" placeholder="名称"><input type="text" class="form-control member_position" name="member_position" value="" placeholder="职位" ></div>'
+
+  $('.team').append(string);
+}
+
+//表单校验
+var pdfType = /pdf.*/;
+var imageType = /image.*/;
+var imageMaxSize = 2*1024*1024;
+var whitePaperMaxSize = 2*1024*1024*10;
+var flag = true;
+
+$('#form1').validator({
+	theme:'bootstrap',
+  validClass: "has-succes",
+  invalidClass: "has-error",
+  bindClassTo: ".form-group",
+
+  rules: {
+    imageTypeAndSize:function(element, params,field) {
+      var filepath = element.files[0];
+      if (!filepath.type.match(imageType) || filepath.size > imageMaxSize){
+        return "上传有误";
+      }else {
+        return true;
+      }
+    },
+    pdfTypeAndSize:function(element, params,field) {
+      var filepath = element.files[0];
+      if (element.files.length == 0) {
+        flag = true
+        return true
+      }
+      if (!filepath.type.match(pdfType) || filepath.size > imageMaxSize){
+        flag = false
+        return false
+      }else {
+        flag = true;
+        return true
+      }
+    },
+    pdf:function(element, params,field){
+      if (flag) {
+        return true
+      }else{
+        return '请上传不大于20M的PDF文件';
+      }
+    },
+
+    teamNumber:function(element, params, field){
+      var numbers = element;
+      if (numbers%3 == 0) {
+        return true;
+      }else {
+        return '请完整地填写团队成员的信息'
+      }
+    }
+
+  },
+
+  fields: {
+		'project_name': 'required',
+		'project_big_name': 'required',
+    'project_type': 'required',
+		'fundraising_time': 'date',
+		'currency_count': 'required;integer',
+		'compay_website': 'required;url',
+		'block_browser': 'url',
+		'project_content': 'required',
+    'currency_circulation': 'required;integer',
+    'white_paper_file_name': 'pdf',
+  }
+});
+
+
+// 表单提交
+
+
 var allFile = {
   'projectLogo': '',
   'memberPic': [],
   'whitePaper': ''
 }
-var t
-// 表单提交
+
+
 function doUpload(e){
   var formData = new FormData();
   var file = e.files[0];
@@ -194,6 +197,7 @@ function doUpload(e){
         datType: "json",
         processData: false,  // 不处理数据
         contentType: false,   // 不设置内容类型
+
         success:function(data){
             if (t.className == 'project_logo') {
               allFile.projectLogo = (data.datas[0])
@@ -208,18 +212,21 @@ function doUpload(e){
             alert("错误！！");
         }
   });
+
+
+
 }
 
 
 $('.submit_control').on('click', function(){
 
   var team = [];
-  var temp = {};
   teamLength = allFile.memberPic.length;
   memberName = $(" input[name='member_name']");
   memberPosition = $(" input[name='member_position']");
 
   for (var i = 0; i < teamLength; i++) {
+    var temp = {};
     temp.picHref = allFile.memberPic[i];
     temp.name = memberName[i].value;
     temp.position = memberPosition[i].value;
@@ -247,18 +254,17 @@ $('.submit_control').on('click', function(){
   $.ajax({
       type: 'POST',
       url: uri,
-      headers: {
-          "request-id": guid() + new Date().getTime()
-      },
       data: JSON.stringify(data),
       dataType : 'json',
       contentType: 'application/json; charset=UTF-8',
       success: function (result) {
        if(result.code=="0"){
-          alert("保存成功");
+          // TODO: 跳到项目页
+          console.log('ok')
         }
         else{
           alert("保存失败");
+          return false
         }
       },
       error: function (err) {
