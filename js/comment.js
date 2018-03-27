a74f249baafbb947de49d1ca4a1e15e40fada0eb
liuzhixian <956295973@qq.com>
2018-03-27 16:09:17 +0800

    链库

---
+++ js/comment.js
@@ -0,0 +1,23 @@
+//举报弹出框
+$('#comments .report_comment').on('click',function (e) {
+    console.log($(e.currentTarget))
+    layer.open({
+        type: 1,
+        shade:0,
+        title: '选择举报原因',
+        skin: 'layui-layer-report', //加上边框
+        area: ['442px', '200px'], //宽高
+        content: $("#template-report-popup").html()
+    });
+});
+//点击引用
+$('#comments .reply_comment').on('click',function (e) {
+    $(".reply-comment").fadeIn()
+    console.log($(e.currentTarget))
+});
+//点击关闭
+$(".review-comment-form .lnk-close").on('click',function (e) {
+    $(".reply-comment").fadeOut()
+    console.log($(e.currentTarget))
+});
+
