f4b89af720c1e4fd95b6f35ab09d42c78fe07ab8
wuzhao <liwuzhao97@gmail.com>
2018-04-11 11:29:10 +0800

    fix bug

--- js/chain.js
+++ js/chain.js
@@ -273,6 +273,7 @@ $(window).resize(function () {
 		$(".search-result-img .inner-img-wrap").css('height', imgZ*270/230);
 })
 
+
 // 移动端的分类筛选显示
 $(function(){
 	if (($(window).width() <= 767)) {
@@ -283,4 +284,14 @@ $(function(){
 
 $('.load-category').on('click',function(){
 	$('.category').fadeToggle()
+})
+
+$(window).resize(function () {
+	if (($(window).width() <= 767)) {
+		$('.category').css('display','none')
+		$('.load-category-box').css('display','')
+	}else{
+		$('.category').css('display','')
+		$('.load-category-box').css('display','none')
+	}
 })
