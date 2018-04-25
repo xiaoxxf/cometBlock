785e3ab9a862891690afacd066d4a2b08034207e
wuzhao <liwuzhao97@gmail.com>
2018-04-25 23:40:18 +0800

    hot article html

--- js/hot-article.js
+++ js/hot-article.js
@@ -4,11 +4,32 @@ var userinfo = JSON.parse(localStorage.getItem('userinfo'))
 var article_pgae = 1
 // 渲染评测
 $(function(){
+
   article_pgae = 1
   var uri = 'blockchain/quaryReview?currentPage=' + article_pgae + '&pageSize=4&type=2&like=1'
   doJavaGet(uri, function(result){
       $('.article-top-box').html("");
-      // debugger
+      var content_length = null
+
+      for (var i = 0; i < result.datas.length; i++) {
+        result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"")
+
+        if ($(window).width() < 767) {
+          content_length = 55
+        }else{
+          content_length = 300
+        }
+
+        if (result.datas[i].textContent.length > content_length) {
+          result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
+        }
+
+        // if (result.datas[i].textTitle.length > 30) {
+        //   result.datas[i].textTitle = result.datas[i].textTitle.substring(0,30) + "..."
+        // }
+
+      }
+
       var tpl = document.getElementById('article_tpl').innerHTML;
       var content = template(tpl, {list: result.datas});
       $('.article-top-box').append(content)
