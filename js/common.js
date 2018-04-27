ef5f55e4bd44274f54f2855b5f6f374143eb267e
wuzhao <liwuzhao97@gmail.com>
2018-04-27 09:27:18 +0800

    article-new

--- js/common.js
+++ js/common.js
@@ -108,7 +108,9 @@ function Loginout(){
 
 //头部尾部全局加载
 $.get("footer-tpl.html",function(data){
-    $(".block-comet-main-wrap").append(data);
+    if ( !window.location.pathname == '/cometBlock/article-new.html') {
+      $(".block-comet-main-wrap").append(data);
+    }
 });
 $.get("header-tpl.html",function(data){
     $(".navbar-fixed-container-hook").html(data);
