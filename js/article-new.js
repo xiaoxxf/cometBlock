de56b0dcb13ab2edb71e4f9b037ca6fce4b8f797
wuzhao <liwuzhao97@gmail.com>
2018-04-27 09:27:24 +0800

    article-new

--- js/article-new.js
+++ js/article-new.js
@@ -71,22 +71,6 @@ editor.create();
 $('.w-e-text-container').attr('style','height:auto;');
 
 
-window.wangEditor.fullscreen = {
-	// editor create之后调用
-	init: function(editorSelector){
-		$(editorSelector + " .w-e-toolbar").append('<div class="w-e-menu"><a class="_wangEditor_btn_fullscreen" href="###" onclick="window.wangEditor.fullscreen.toggleFullscreen(\'' + editorSelector + '\')">全屏</a></div>');
-	},
-	toggleFullscreen: function(editorSelector){
-		$(editorSelector).toggleClass('fullscreen-editor');
-		if($(editorSelector + ' ._wangEditor_btn_fullscreen').text() == '全屏'){
-			$(editorSelector + ' ._wangEditor_btn_fullscreen').text('退出全屏');
-		}else{
-			$(editorSelector + ' ._wangEditor_btn_fullscreen').text('全屏');
-		}
-	}
-};
-
-
 // 修改菜单栏样式
 $('.w-e-toolbar').css(
   {
