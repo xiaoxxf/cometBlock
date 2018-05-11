428f95c961e025923dd13f1c70d71dcead40418c
wuzhao <liwuzhao97@gmail.com>
2018-05-11 16:43:04 +0800

    topic-show.html

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,12 @@
-var userId = $.cookie('userid');//获取userid
-var userinfo = JSON.parse(localStorage.getItem('userinfo'))
-var localCookieWechatInfo = $.cookie('wechatInfo');
-localCookieWechatInfo = localCookieWechatInfo == undefined ? localCookieWechatInfo : JSON.parse(localCookieWechatInfo);
+// var userId = $.cookie('userid');//获取userid
+// var userinfo = JSON.parse(localStorage.getItem('userinfo'))
+// var localCookieWechatInfo = $.cookie('wechatInfo');
+// localCookieWechatInfo = localCookieWechatInfo == undefined ? localCookieWechatInfo : JSON.parse(localCookieWechatInfo);
+var ui = {
+	"noData": false,
+	"noMoreData": false,
+	"loading": false
+}
 
 $(function(){
   var uri = 'blockchain/quaryProjetList?currentPage=1&pageSize=15'
@@ -108,105 +113,108 @@ $(function(){
     var tpl = document.getElementById('topic_tpl').innerHTML;
     var content = template(tpl, {list: result.datas});
     $('.hot_main_area').append(content);
-    $($('.hot_main_area').find('a')[0]).addClass('hot-span-item')
+    $($('.hot_main_area').find('a')[0]).addClass('hot-span-item');
+    $($('.hot_main_area').find('a')[0]).click();
+
   })
 
 })
 
-var article_loading = false
-var article_pgae = 1
-// 渲染评测
-$(function(){
-  article_pgae = 1
-  article_loading = true
-  var uri = 'blockchain/quaryReview?currentPage=' + article_pgae + '&pageSize=15&type=2&like=1'
+var topicId = ''
+var topic_article_page = 1;
+// 渲染专题下的文章
+$('.hot_main_area').on('click', 'a', function(e){
+  topic_article_page = 1;
+  var self = $(e.currentTarget);
+  topicId = self.data('topicid');
+  var uri = 'topic/quaryArticle?topicId=' + topicId + '&currentPage=' + topic_article_page + '&pageSize=12'
+
   doJavaGet(uri, function(result){
-      // $('.hot_review_region').html("");
-
-      // 限制标题和内容长度
+    if (result.datas.length == 0) {
+      $('.hot_review_region').html('');
+      $('.read-more').css('display','none');
+    }else{
+      $('.read-more').css('display','');
+      // 限制内容长度
       for (var i = 0; i < result.datas.length; i++) {
-        result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"")
 
-        var content_length = null
-        if ($(window).width() < 767) {
-          content_length = 55
-        }else{
-          content_length = 120
-        }
-
-        if (result.datas[i].textContent.length > content_length) {
-          result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
-        }
-
-        // if (result.datas[i].textTitle.length > 30) {
-        //   result.datas[i].textTitle = result.datas[i].textTitle.substring(0,30) + "..."
-        // }
-
-      }
-
-      var tpl = document.getElementById('hot_article_tpl').innerHTML;
-      var content = template(tpl, {list: result.datas});
-      $('.hot_review_region').append(content)
-
-      var imgW = $(".hot_zone .article-detail .article-icon").width();
-      $(".hot_zone .article-detail .article-icon").css('height',imgW*270/230);
-
-      // $('.article-count').html(result.count)
-      article_loading = false
-  })
-})
-
-var noMoreData = false
-// 评测加载更多
-$('.read-more').on('click',function(){
-
-  article_pgae+=1
-  var uri = 'blockchain/quaryReview?currentPage=' + article_pgae + '&pageSize=15&type=2&like=1'
-
-  if (!noMoreData && !article_loading) {
-    $('.read-more').html('加载中...')
-
-    doJavaGet(uri,function(result){
-
-      if (result.datas.length == 0) {
-        noMoreData = true
-        $('.read-more').html('已无更多数据')
-      }else{
-
-        var content_length = null
-        if ($(window).width() < 767) {
-          content_length = 55
-        }else{
-          content_length = 120
-        }
-
-        // 限制标题和内容长度
-        for (var i = 0; i <result.datas.length; i++) {
+        if (result.datas[i].textContent) {
           result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"")
+
+          var content_length = null
+          if ($(window).width() < 767) {
+            content_length = 55
+          }else{
+            content_length = 120
+          }
 
           if (result.datas[i].textContent.length > content_length) {
             result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
           }
-          // if (result.datas[i].textTitle.length > 30) {
-          //   result.datas[i].textTitle = result.datas[i].textTitle.substring(0,30) + "..."
-          // }
-
         }
-
-
-        var tpl = document.getElementById('hot_article_tpl').innerHTML;
-        var content = template(tpl, {list: result.datas});
-        $('.hot_review_region').append(content)
-
-        var imgW = $(".hot_zone .article-detail .article-icon").width();
-        $(".hot_zone .article-detail .article-icon").css('height',imgW*270/230);
-        $('.read-more').html('阅读更多')
       }
 
-    })
-  }
+      var tpl= document.getElementById('hot_article_tpl').innerHTML;
+      var content = template(tpl, {list: result.datas});
+      $('.hot_review_region').html('');
+      $('.hot_review_region').append(content);
+      $('.read-more').css('display','');
+      $('.read-more').text('阅读更多');
+    }
+
+    ui.loading = false;
+
+  })
+
 })
 
+// 专题下的文章加载更多
+$('.read-more').on('click',function(){
+  if (ui.loading || ui.noMoreData ) {
+    return
+  }
+
+  ui.loading = true;
+  $('.read-more').text('加载中...')
+  topic_article_page++
+  var uri = 'topic/quaryArticle?topicId=' + topicId + '&currentPage=' + topic_article_page + '&pageSize=12'
+
+  doJavaGet(uri, function(result){
+    if (result.datas.length == 0) {
+      ui.noMoreData = true;
+      $('.read-more').text('已无更多数据');
+
+    }else{
+      // 限制内容长度
+      for (var i = 0; i < result.datas.length; i++) {
+
+        if (result.datas[i].textContent) {
+          result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"")
+
+          var content_length = null
+          if ($(window).width() < 767) {
+            content_length = 55
+          }else{
+            content_length = 120
+          }
+
+          if (result.datas[i].textContent.length > content_length) {
+            result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
+          }
+        }
+      }
+
+      var tpl= document.getElementById('hot_article_tpl').innerHTML;
+      var content = template(tpl, {list: result.datas});
+      $('.hot_review_region').append(content);
+      $('.read-more').text('阅读更多');
+
+    }
+
+    ui.loading = false;
+
+  })
+})
 
 // 长文点赞
 $(".hot_review_region").on('click','.like-button',function (e) {
