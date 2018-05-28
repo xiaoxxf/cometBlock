133f8e13263ef8256cc5926efa4030d28c80ab40
wuzhao <liwuzhao97@gmail.com>
2018-05-28 19:19:06 +0800

    关注

--- js/attention.js
+++ js/attention.js
@@ -11,9 +11,10 @@ window.onload = function(){
 var tpl_id = 'all_dynamic';
 var currentPage_dynamics = 1;
 var pageSize_dynamics = 6;
-var like = '';
-var current_attentionId = null; //当前要加载的关注的对象
-var current_attentionType = null; //当前要加载的关注的类型
+var like = ''; // 加载推荐的动态
+var type = null; //加载关注动态 1 -> 关注人， 2 -> 关注的专题, 4 -> 关注的项目
+// var current_attentionId = null; //当前要加载的关注的对象
+// var current_attentionType = null; //当前要加载的关注的类型
 
 // 加载全部动态
 $('.side_menu').on('click','.load_all_user_dynamic', function(){
@@ -21,11 +22,11 @@ $('.side_menu').on('click','.load_all_user_dynamic', function(){
 		return
 	}
 	$('.side_menu span').removeClass('dynamic_menu_on_focus');
-	$('.side_menu li').removeClass('attention_dynamic_on_focus');
+	// $('.side_menu li').removeClass('attention_dynamic_on_focus');
 	$('.load_all_user_dynamic').addClass('dynamic_menu_on_focus');
 	like = '';
-	current_attentionId = null;
-	current_attentionType = null;
+	type = null;
+	tpl_id = 'all_dynamic';
 	getAllUserDynamic();
 })
 
@@ -35,44 +36,55 @@ $('.side_menu').on('click','.load_recommend_dynamic', function(){
 		return
 	}
 	$('.side_menu span').removeClass('dynamic_menu_on_focus');
-	$('.side_menu li').removeClass('attention_dynamic_on_focus');
+	// $('.side_menu li').removeClass('attention_dynamic_on_focus');
 	$('.load_recommend_dynamic').addClass('dynamic_menu_on_focus');
 	like = 1;
-	current_attentionId = null;
-	current_attentionType = null;
+	type = null;
+	tpl_id = 'all_dynamic';
 	getAllUserDynamic();
 })
 
-
 // 加载关注的人的动态
-$('.follow_people_list').on('click', '.load_people_dynamic', function(e){
+$('.side_menu').on('click', '.load_follow_people', function(e){
+	if (ui.loading) {
+		return
+	}
 	$('.side_menu span').removeClass('dynamic_menu_on_focus');
-	$('.side_menu li').removeClass('attention_dynamic_on_focus');
-	$(this).addClass('attention_dynamic_on_focus');
-	var self = $(this);
-			current_attentionId = self.data('followingid'),
-			current_attentionType = 1;
+	$('.load_follow_people').addClass('dynamic_menu_on_focus');
+	type = 1;
+	like = '';
+	tpl_id = 'all_dynamic';
+	$('.no-data').html('你还没有关注任何用户')
+
 	getAllUserDynamic();
 })
 
 // 加载关注的专题的动态
-$('.follow_topic_list').on('click', '.load_topic_dynamic',function(e){
+$('.side_menu').on('click', '.load_follow_topic',function(e){
+	if (ui.loading) {
+		return
+	}
 	$('.side_menu span').removeClass('dynamic_menu_on_focus');
-	$('.side_menu li').removeClass('attention_dynamic_on_focus');
-	$(this).addClass('attention_dynamic_on_focus');
-	current_attentionId = $(this).data('topicid'),
-	current_attentionType = 2;
+	$('.load_follow_topic').addClass('dynamic_menu_on_focus');
+	type = 2;
+	like = '';
+	tpl_id = 'following_topic_dynamic';
+	$('.no-data').html('你还没有关注任何专题，去看看有什么<a href="topic-index.html">专题</a>')
+
 	getAllUserDynamic();
 })
 
 // 加载关注的项目的动态
-$('.follow_project_list').on('click', '.load_project_dynamic' ,function(e){
+$('.side_menu').on('click', '.load_follow_project' ,function(e){
+	if (ui.loading) {
+		return
+	}
 	$('.side_menu span').removeClass('dynamic_menu_on_focus');
-	$('.side_menu li').removeClass('attention_dynamic_on_focus');
-	$(this).addClass('attention_dynamic_on_focus');
-	var self = $(this);
-			current_attentionId = self.data('projectid'),
-			current_attentionType = 3;
+	$('.load_follow_project').addClass('dynamic_menu_on_focus');
+	type = 3;
+	like = '';
+	tpl_id = 'all_dynamic';
+	$('.no-data').html('你还没有关注任何项目，去看看有什么<a href="chain.html">项目</a>')
 	getAllUserDynamic();
 })
 
@@ -85,78 +97,63 @@ function getAllUserDynamic(){
   ui.loading = true;
   ui.noMoreData = false;
 	$(".no-more-hook").css('display','none');
-	// 加载关注的人、项目、专题动态
-	if (current_attentionId && current_attentionType) {
-		switch (current_attentionType) {
-			// 加载关注人的动态
-			case 1:
-			var uri = 'blockchain/quaryReviewByUser?currentPage=' + currentPage_dynamics
-								+ '&pageSize=' + pageSize_dynamics + '&creator=' + current_attentionId;
-			break;
-			// 加载关注专题的动态
-			case 2:
-				var uri = 'topic/quaryArticle?topicId=' + current_attentionId
-									+ '&currentPage=' + currentPage_dynamics +'&pageSize=' + pageSize_dynamics
-				break;
-			// 加载关注项目的动态
-			case 3:
-				var uri = 'attention/quaryReviewById?projectId=' + current_attentionId + '&currentPage='
-										+ currentPage_dynamics + '&pageSize=' + pageSize_dynamics
-				break;
-			default:
 
-		}
+	// 加载关注的
+	if (type) {
+		var uri = 'attention/all?creator=' + userId +  '&password=' + userinfo.userPwd + '&currentPage='
+							+ currentPage_dynamics + '&pageSize=' + pageSize_dynamics
+							+ '&type=' + type
 	}
-	// 加载全部动态或推荐动态
+	// 加载推荐的或推荐的
 	else{
 		var uri = 'blockchain/quaryReviewByUser?currentPage=' + currentPage_dynamics + '&pageSize=' + pageSize_dynamics
 							+ '&like=' + like
-
 	}
 
   // 首次加载
   // $('.read-more').css('display','none')
-  $(".refresh_load").fadeIn();
-  $('.dynamic_region').html('');
+	$('.dynamic_region').html('');
+  $(".refresh_load").css('display','');
+	$('.no-data').css('display','none');
 
   doJavaGet(uri,function(result){
     if (result.datas.length == 0) {
-      ui.noMoreData = true
-      $(".refresh_load").hide();
+      ui.noMoreData = true;
+			$(".refresh_load").css('display','none');
+			$('.no-data').css('display','');
       // $('.read-more').css('display','block')
       // $('.read-more').html('已无更多数据')
-      return
-    }
+      // return
+    }else{
+	    // 限制内容长度
+	    for (var i = 0; i < result.datas.length; i++) {
 
-    // 限制内容长度
-    for (var i = 0; i < result.datas.length; i++) {
+	      if (result.datas[i].textContent) {
+					// 去除HTML标签和无用的空格
+	        result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"").replace(/^\s+|\s+$/g,"")
 
-      if (result.datas[i].textContent) {
-				// 去除HTML标签和无用的空格
-        result.datas[i].textContent = result.datas[i].textContent.replace(/<[^>]+>/g,"").replace(/^\s+|\s+$/g,"")
+	        var content_length = null
+	        if ($(window).width() < 767) {
+	          content_length = 85
+	        }else{
+	          content_length = 150
+	        }
 
-        var content_length = null
-        if ($(window).width() < 767) {
-          content_length = 85
-        }else{
-          content_length = 150
-        }
+	        if (result.datas[i].textContent.length > content_length) {
+	          result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
+	        }
+	      }
 
-        if (result.datas[i].textContent.length > content_length) {
-          result.datas[i].textContent = result.datas[i].textContent.substring(0,content_length) + "..."
-        }
-      }
+	    }
 
-    }
+			var tpl = document.getElementById(tpl_id).innerHTML;
+			var content = template(tpl, {list: result.datas});
 
-
-    var tpl = document.getElementById(tpl_id).innerHTML;
-    var content = template(tpl, {list: result.datas});
-
-    $(".refresh_load").hide();
-    $('.dynamic_region').append(content)
-    // $('.read-more').css('display','block')
-    // $('.read-more').html('点击加载更多')
+			$(".refresh_load").css('display','none');
+	    $('.dynamic_region').append(content)
+	    // $('.read-more').css('display','block')
+	    // $('.read-more').html('点击加载更多')
+		}
     ui.loading = false;
   })
 }
@@ -167,29 +164,13 @@ function loadMoreDynamic(){
   ui.loading = true;
   ui.noMoreData = false;
 
-	// 加载关注的人、项目、专题动态
-	if (current_attentionId && current_attentionType) {
-		switch (current_attentionType) {
-			// 加载关注人的动态
-			case 1:
-			var uri = 'blockchain/quaryReviewByUser?currentPage=' + currentPage_dynamics
-								+ '&pageSize=' + pageSize_dynamics + '&creator=' + current_attentionId;
-			break;
-			// 加载关注专题的动态
-			case 2:
-				var uri = 'topic/quaryArticle?topicId=' + current_attentionId
-									+ '&currentPage=' + currentPage_dynamics +'&pageSize=' + pageSize_dynamics
-				break;
-			// 加载关注项目的动态
-			case 3:
-				var uri = 'attention/quaryReviewById?projectId=' + current_attentionId + '&currentPage='
-									+ currentPage_dynamics + '&pageSize=' + pageSize_dynami
-				break;
-			default:
-
-		}
+	// 加载关注的
+	if (type) {
+		var uri = 'attention/all?creator=' + userId +  '&password=' + userinfo.userPwd + '&currentPage='
+							+ currentPage_dynamics + '&pageSize=' + pageSize_dynamics
+							+ '&type=' + type
 	}
-	// 加载全部动态或推荐动态
+	// 加载推荐的或推荐的
 	else{
 		var uri = 'blockchain/quaryReviewByUser?currentPage=' + currentPage_dynamics + '&pageSize=' + pageSize_dynamics
 							+ '&like=' + like
@@ -329,76 +310,76 @@ $(".hot_review_region").on('click','.followBtn',function(){
 })
 
 // 侧边栏
-$('.follow_people .load_follow_people').on('click',function(){
-	$('.follow_people_list').toggle();
-
-})
-
-$('.follow_project .load_follow_project').on('click',function(){
-	$('.follow_project_list').toggle();
-})
-
-$('.follow_topic .load_follow_topic').on('click',function(){
-	$('.follow_topic_list').toggle();
-})
+// $('.follow_people .load_follow_people').on('click',function(){
+// 	$('.follow_people_list').toggle();
+//
+// })
+//
+// $('.follow_project .load_follow_project').on('click',function(){
+// 	$('.follow_project_list').toggle();
+// })
+//
+// $('.follow_topic .load_follow_topic').on('click',function(){
+// 	$('.follow_topic_list').toggle();
+// })
 
 // 加载关注的人、项目、专题列表
 
-$(function(){
-	var type = 0;  // 1->人 2->专题 3->项目
-	var noMoreDataFlag = false;
-	var append_class = null;
-	var tpl_id = null;
-	var currentPage_follow = 0;
-	var pageSize_follow = 12;
-	for (var i = 0; i < 3; i++) {
-		type++;
-		currentPage_follow = 0;
-		noMoreDataFlag = false;
-		switch (type) {
-			case 1:
-				append_class = '.follow_people_list';
-				tpl_id = 'follow_people_list_tpl';
-				break;
-			case 2:
-				append_class = '.follow_topic_list'
-				tpl_id = 'follow_topic_list_tpl';
-				break;
-			case 3:
-				append_class = '.follow_project_list'
-				tpl_id = 'follow_project_list_tpl';
-
-				break;
-			default:
-		}
-
-		while(!noMoreDataFlag)
-		{
-			currentPage_follow++;
-			var uri = 'attention/quaryAttentionData?creator=' + userId + '&password=' + userinfo.userPwd
-								+ '&currentPage=' + currentPage_follow + '&pageSize=' + pageSize_follow + '&type=' + type;
-			// 使用同步
-			$.ajax({
-		    url : WebApiHostJavaApi + uri,
-		    type: "get",
-		    async: false,//使用同步的方式,true为异步方式
-		    processData: false,  // 不处理数据
-		    contentType: false,   // 不设置内容类型
-
-		    success:function(res){
-					var tpl= document.getElementById(tpl_id).innerHTML;
-					var content = template(tpl, {list: res.datas});
-					$(append_class).append(content);
-					res.datas.length < pageSize_follow ? noMoreDataFlag = true : noMoreDataFlag = false
-					// noMoreDataFlag = true
-		    },
-
-		  });
-
-		}
-	}
-
-})
+// $(function(){
+// 	var type = 0;  // 1->人 2->专题 3->项目
+// 	var noMoreDataFlag = false;
+// 	var append_class = null;
+// 	var tpl_id = null;
+// 	var currentPage_follow = 0;
+// 	var pageSize_follow = 12;
+// 	for (var i = 0; i < 3; i++) {
+// 		type++;
+// 		currentPage_follow = 0;
+// 		noMoreDataFlag = false;
+// 		switch (type) {
+// 			case 1:
+// 				append_class = '.follow_people_list';
+// 				tpl_id = 'follow_people_list_tpl';
+// 				break;
+// 			case 2:
+// 				append_class = '.follow_topic_list'
+// 				tpl_id = 'follow_topic_list_tpl';
+// 				break;
+// 			case 3:
+// 				append_class = '.follow_project_list'
+// 				tpl_id = 'follow_project_list_tpl';
+//
+// 				break;
+// 			default:
+// 		}
+//
+// 		while(!noMoreDataFlag)
+// 		{
+// 			currentPage_follow++;
+// 			var uri = 'attention/quaryAttentionData?creator=' + userId + '&password=' + userinfo.userPwd
+// 								+ '&currentPage=' + currentPage_follow + '&pageSize=' + pageSize_follow + '&type=' + type;
+// 			// 使用同步
+// 			$.ajax({
+// 		    url : WebApiHostJavaApi + uri,
+// 		    type: "get",
+// 		    async: false,//使用同步的方式,true为异步方式
+// 		    processData: false,  // 不处理数据
+// 		    contentType: false,   // 不设置内容类型
+//
+// 		    success:function(res){
+// 					var tpl= document.getElementById(tpl_id).innerHTML;
+// 					var content = template(tpl, {list: res.datas});
+// 					$(append_class).append(content);
+// 					res.datas.length < pageSize_follow ? noMoreDataFlag = true : noMoreDataFlag = false
+// 					// noMoreDataFlag = true
+// 		    },
+//
+// 		  });
+//
+// 		}
+// 	}
+//
+// })
 
 // 侧边栏宽度
 $(function(){
