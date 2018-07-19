2d165858e32d925b48079f4474f32e4fef0a7bf7
unknown <2214871244@qq.com>
2018-07-19 16:44:52 +0800

    楼中楼

--- js/comment-page.js
+++ js/comment-page.js
@@ -24,11 +24,18 @@ function ajaxGetLongCommentReview() {
 
     doJavaGet(uri, function(res) {
         if(res != null && res.code == 0) {
-
+		
             commentList = res.datas;
+            for (var i = 0; i< commentList.length; i++ ){
+            	commentList[i].createTime = changeTimeFormat(commentList[i].createTime);
+            	for(var j = 0; j < commentList[i].fedbackList.length; j++){
+            	    commentList[i].fedbackList[j].createTime = changeTimeFormat(commentList[i].fedbackList[j].createTime);
+            	}
+            }
             // console.log(res.datas)
             var commentTpl = $("#template-comment-list").html();
             var teamContent = template(commentTpl, {list: commentList});
+
             $(".comment-list-hook").html(teamContent);
             //if(res.datas.length >=10){
             // 分页数据处理
@@ -93,3 +100,47 @@ $("#paginator").on('click','.goPre',function (e) {
     longCommentCurrentPage = pageNum;
     ajaxGetLongCommentReview();
 })*/
+
+// 转换时间格式
+function changeTimeFormat(dateTimeStamp){
+	  var result;
+	  dateTimeStamp = new Date(dateTimeStamp);
+	    var minute = 1000 * 60;
+	    var hour = minute * 60;
+	    var day = hour * 24;
+	    var halfamonth = day * 15;
+	    var month = day * 30;
+	    var now = new Date().getTime();
+	    var diffValue = now - dateTimeStamp;
+	    if(diffValue < 0){
+	    return;
+	  }
+	    var monthC =diffValue/month;
+	    var weekC =diffValue/(7*day);
+	    var dayC =diffValue/day;
+	    var hourC =diffValue/hour;
+	    var minC =diffValue/minute;
+	    if(monthC>=1){
+	    if(monthC<=12)
+	          result="" + parseInt(monthC) + "月前";
+	else{
+	  result="" + parseInt(monthC/12) + "年前";
+	}
+	}
+	else if(weekC>=1){
+	    result="" + parseInt(weekC) + "周前";
+	}
+	else if(dayC>=1){
+	    result=""+ parseInt(dayC) +"天前";
+	}
+	else if(hourC>=1){
+	    result=""+ parseInt(hourC) +"小时前";
+	}
+	else if(minC>=1){
+	    result=""+ parseInt(minC) +"分钟前";
+	}else{
+	result="刚刚";
+	}
+	    return result;
+	
+}
